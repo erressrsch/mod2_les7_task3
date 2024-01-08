3a8d96c413b0e4a87e202321039a85840c468c5f
Szymon Chyc <erressrsch@gmail.com>
2024-01-08 23:08:09 +0100

    Add project files.

---
+++ mod2_les7_task3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace mod2_les7_task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 14;
+
+            if (a >= 0)
+            {
+                Console.WriteLine($"{a} is a positive number");
+            }
+            else
+            {
+                Console.WriteLine($"{a} is a negative number");
+            }
+        }
+    }
+}
